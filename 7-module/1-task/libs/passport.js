f7b7734af69f932aab4375d74e38da1100728657
Sergey Zelenov <mail@zelenov.su>
2019-04-22 16:19:27 +0200

    rm

--- 7-module/1-task/libs/passport.js
+++
@@ -1,7 +0,0 @@
-const passport = require('koa-passport');
-
-const localStrategy = require('./strategies/local');
-
-passport.use(localStrategy);
-
-module.exports = passport;
--- 7-module/1-task/libs/strategies/local.js
+++
@@ -1,9 +0,0 @@
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('../../models/User');
-
-module.exports = new LocalStrategy(
-  { usernameField: 'email', session: false, },
-  function(email, password, done) {
-    done(null, false, 'Стратегия подключена, но еще не настроена');
-  }
-);
